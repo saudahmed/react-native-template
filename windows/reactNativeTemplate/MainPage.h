12b3e1755fa386d042223433d48448fb978675ba
Saud Ahmed <mahmedsaud@gmail.com>
2024-01-30 23:59:50 +0100

    Windows project init

---
+++ windows/reactNativeTemplate/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::reactNativeTemplate::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::reactNativeTemplate::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
